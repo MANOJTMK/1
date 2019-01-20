d586a944236be31bb71665fb12685ddebccec87b
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-01-20 18:36:11 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+	int t;
+	printf("test case");
+	scanf("%d",&t);
+	{
+		int i,n;
+		printf("enter the values");
+		scanf("%d",&n);
+		for(i=1;i<=n;i++)
+		{
+			printf("hello");
+			printf("\n");
+		}
+		t--;
+	}
+		
+		return 0;
+}
+
